51f8b916b509f0964d70f41ce6c3f202ad97e737
u-Hoshi <yuta0000707@yahoo.co.jp>
2021-05-20 09:53:21 +0900

    環境のセットアップ

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  // 子の中に書いたものがwebpackの監視対象となる
+  entry: {
+    // エントリーポイントを記述
+    bundle: './src/index.ts',
+  },
+  output: {
+    // バンドルしたものをどこに出力するか
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devServer: {
+    // どこのファイルが変わったらホットリロードするかを決める
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\ts.$/,
+      },
+    ],
+  },
+};
