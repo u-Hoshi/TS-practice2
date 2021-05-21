6c75c57c2e1994c94f24586676a30450dfe4cc01
u-Hoshi <yuta000707@yahoo.co.jp>
2021-05-21 10:25:37 +0900

    husky

--- src/world.ts
+++ src/world.ts
@@ -8,7 +8,7 @@ export default class World {
   }
 
   //外部から呼び出せるメソッドを定義
-  public sayHello(elem: HTMLElement | null) {
+  public sayHello(elem) {
     if (elem) {
       elem.innerText = this.message
     }
