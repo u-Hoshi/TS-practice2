93644e7aef12a5198f5896456e415791b9200154
u-Hoshi <yuta000707@yahoo.co.jp>
2022-02-04 23:59:01 +0900

    オブジェクトに関して

--- ORilly/src/index.ts
+++ ORilly/src/index.ts
@@ -12,3 +12,24 @@ const d = c.apple * 4
 
 //es2020以上であれば使える
 // const f = 1234n + 5678n //bigint
+
+// symbol
+const h = Symbol('a')
+console.log(h)
+
+const j = {
+  a: 'x',
+}
+console.log(j)
+
+// let obj: {
+//   [key: string]: string // インデックスシグネチャ
+// } // オブジェクトリテラル
+
+// OK
+// obj = {
+//   id: '123',
+//   token: 'hogetoken',
+//   name: 'fuganame',
+// }
+// console.log(obj)
