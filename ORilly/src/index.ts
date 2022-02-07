7b74124290e8cf27072a795a779616daf2084329
u-Hoshi <yuta000707@yahoo.co.jp>
2022-02-07 23:54:36 +0900

    タプルに関して

--- ORilly/src/index.ts
+++ ORilly/src/index.ts
@@ -73,3 +73,35 @@ const bb: CatAndDog = {
 }
 
 console.log(aa, bb)
+
+// タプル
+const aaa: [number] = [1]
+
+// [名前、苗字、生まれ年]のタプル
+// eslint-disable-next-line prefer-const
+let bbb: [string, string, number] = ['queen', 'elizabeth', 10963]
+
+// bbb = ['malcom', 'gladwell', 'ii', 2000] // 型 'string' を型 'number' に割り当てることはできません。ts(2322)
+
+console.log(aaa, bbb)
+
+/* タプルは省略可能な要素もサポートしている。オブジェクト同様に「？」を「オプション」という意味で使用する */
+
+// 鉄道運賃の配列。方向によって異なる場合がある
+const trainFaraes: [number, number?][] = [[3, 75], [8.25], [10, 55]]
+// ↑は↓と同等である
+const moreTrainFares: ([number] | [number, number])[] = [[4, 2], [3]]
+
+console.log(trainFaraes, moreTrainFares)
+
+/* またタプルは可変長の要素もサポートしている。これを使用すると最小限の長さについて制約を持ったタプルを型付けできる */
+
+// 少なくとも1つの要素を持つ、文字列のリスト
+const friends: [string, ...string[]] = ['sara', 'hoge']
+
+// 不均一なリスト
+const list: [number, boolean, ...string[]] = [1, false, 'a', 'b']
+
+console.log(friends, list)
+
+/* タプルによって型付けするリストの長さを限定することができる。従来の配列より安全を得ることができる */
