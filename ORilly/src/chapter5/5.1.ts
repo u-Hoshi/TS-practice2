978e29123c36411a83929596080b98a7888b04d5
u-Hoshi <yuta000707@yahoo.co.jp>
2022-02-23 23:39:00 +0900

    チェスのクラス作成

--- ORilly/src/chapter5/5.1.ts
+++ ORilly/src/chapter5/5.1.ts
@@ -1,1 +1,18 @@
 // クラスと継承
+
+// チェスのゲームを表す
+class Game {}
+
+// チェスの駒
+class Piece {}
+
+//駒の位置
+class Position {}
+
+// 6種類の駒
+class King extends Piece {}
+class Queen extends Piece {}
+class Bishop extends Piece {}
+class Knight extends Piece {}
+class Rook extends Piece {}
+class Pawn extends Piece {}
