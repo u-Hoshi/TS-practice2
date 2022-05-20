a9dc60e4f1eb04046937c13a46ee55d99b7fccba
u-Hoshi <yuta000707@yahoo.co.jp>
2022-05-20 09:59:05 +0900

    安全な型に関して

--- ORilly/src/chapter6/6.1.ts
+++ ORilly/src/chapter6/6.1.ts
@@ -33,3 +33,34 @@ const legacyUser: LegacyUser = {
 // deleteUser(legacyUser)
 
 // 関数の変性
+function isBig(n: number) {
+  if (n >= 100) {
+    return true
+  }
+}
+
+// 高度なオブジェクト型
+
+type APIResponse = {
+  user: {
+    userId: string
+    friendList: {
+      count: number
+      firends: {
+        firseName: string
+        lastName: string
+      }[]
+    }
+  }
+}
+
+type FriendList = APIResponse['user']['friendList']
+// キーを指定して型を取得することができる
+
+type Friend = FriendList['firends'][number]
+//[number]はキーをして配列型にアクセスするための方法
+
+//6.3.1.2 keyof演算子
+// keyofを使うとオブジェクトの全てのキーを文字列リテラル型の合併として取得できる
+type ResponseKeys = keyof APIResponse
+type UserKeys = keyof APIResponse['user']
