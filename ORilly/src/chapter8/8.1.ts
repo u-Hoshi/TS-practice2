29e47e685b69f57a90ed5a7bdda6a5da91838d35
u-Hoshi <yuta000707@yahoo.co.jp>
2022-05-28 20:58:18 +0900

    コールバック

--- ORilly/src/chapter8/8.1.ts
+++ ORilly/src/chapter8/8.1.ts
@@ -1,1 +1,15 @@
 // 非同期
+// コールバック地獄
+async1((err1, res1) => {
+  if (res1) {
+    async2(res1,(err2, res2) => {
+      if (res2) {
+        async3(res1,(err3, res3) => {
+          2f (res2) {
+          ///
+          }
+        })
+      }
+    })
+  }
+})
